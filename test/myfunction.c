35ca925516460acfdc756eb05f2e260e282b44e7
ruixiangfeng <1784905723@qq.com>
2019-10-27 07:13:23 -0700

    commit week7-test-1

---
+++ test/myfunction.c
@@ -0,0 +1,42 @@
+void Signout-1()
+{
+	
+}
+
+void Create-1(
+{
+}
+
+void WriteFile-1();
+void ReadFile-1();
+void Modify-1();
+void View-1();
+void obtain-1(int a)
+{
+	if(a == 0)
+	{
+		Signout-1();
+     }
+    else if (a == 1)
+	{
+		Create-1();
+	}
+    else if (a == 2)
+	{
+		WriteFile-1();
+	}
+	else if (a == 3)
+	{
+		ReadFile-1();
+	}
+	else if (a == 4)
+	{
+		Modify-1();
+	}
+	else if (a == 5)
+	{
+		View-1();
+	}
+
+}
+
---
+++ test/mylib.h
@@ -0,0 +1,8 @@
+
+void obtain-1(int a);
+void Signout-1();
+void Create-1();
+void WriteFile-1();
+void ReadFile-1();
+void Modify-1();
+void View-1();
