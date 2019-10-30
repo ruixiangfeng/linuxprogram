d83aac05d6163a4f9d38e5df027d7ba796f2ee05
ruixiangfeng <1784905723@qq.com>
2019-10-29 20:01:01 -0700

    second commit week8

---
+++ week8/code/exam/test-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+int main()
+
+{
+
+printf("%s\n",getenv("PATH"));
+
+return 0;
+
+}
---
+++ week8/code/exam/test-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+
+ 
+
+{
+
+extern char **environ;// 声明就能用，不用定义
+
+int i = 0;
+
+while (NULL != environ[i])
+
+{
+
+printf("%s\n", environ[i]);
+
+i++;
+
+}
+
+
+return 0;
+}
