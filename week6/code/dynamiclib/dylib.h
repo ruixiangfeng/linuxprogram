cd7c91021a37fd4929b06627838c8fa7e456d3a4
ruixiangfeng <1784905723@qq.com>
2019-10-29 19:34:38 -0700

    renew week8

--- week6/code/dynamiclib/dylib.h
+++
@@ -1,4 +0,0 @@
-int max(int *a,int n);
-int sum(int *a,int n);
-int show(int *a,int n);
-int init(int *a,int n);
--- week6/code/staticlib/mylib.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int max(int *a,int n);
-int sum(int *a,int n);
-int show(int *a,int n);
-int init(int *a,int n);
-
-
